ac1a09160f842b96f8212baea8d46e7d6325db92
Alif1507 <m.alifwahyudi2007@gmail.com>
2025-02-25 14:14:54 +0700

    add somthing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
+import { motion } from 'motion/react'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <motion.div animate={{ translateY: -900 }} transition={{ duration: 1.2, ease: [0.39, 0.24, 0.15, 0.12] }} className='bg-black w-screen h-screen fixed top-0 left-0'>.</motion.div>
+    <motion.div animate={{ translateY: -900 }} transition={{ duration: 1, ease: [0.39, 0.24, 0.15, 0.12] }} className='bg-amber-700 w-screen h-screen fixed top-0 left-0'>.</motion.div>
       <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
@@ -13,3 +16,4 @@ function App() {
 }
 
 export default App
+  
